152ee7d022e2a341c2d78ba0f91b1dd9374e1603
ricoen <rico.enrayan@gmail.com>
2021-06-17 14:45:51 +0700

    Change some lines

--- dec-tree.py
+++ dec-tree.py
@@ -1,5 +1,6 @@
 import mlpack
 from mlpack import decision_tree
+from mlpack import nbc
 import pandas as pd
 import numpy as np
 
@@ -20,16 +21,18 @@ training_labels = output["training_labels"]
 test_set = output["test"]
 test_labels = output["test_labels"]
 
-output = mlpack.decision_tree(training=training_set,
-                              labels=training_labels,
-                              print_training_accuracy=True,
-                              maximum_depth=5,
-                              minimum_leaf_size=10)
-decision_tree = output["output_model"]
+param_dec_tree = decision_tree(training=training_set,
+                                      labels=training_labels,
+                                      print_training_accuracy=True,
+                                      maximum_depth=5,
+                                      minimum_leaf_size=10)
+dt_model = param_dec_tree["output_model"]
 
-output = mlpack.decision_tree(input_model=decision_tree, test=test_set)
+predict_dt = decision_tree(input_model=dt_model, test=test_set)
 
-correct = np.sum(
-    output["predictions"] == np.reshape(test_labels, (test_labels.shape[0], )))
-print(str(correct) + " correct out of " + str(len(test_labels)) + " (" + 
-    str(100 * float(correct) / float(len(test_labels))) + "%).")
+correct_dec_tree = np.sum(
+    predict_dt["predictions"] == np.reshape(test_labels, (
+        test_labels.shape[0], )))
+print("Decision tree result: " + str(correct_dec_tree) + " correct out of " + 
+      str(len(test_labels)) + " (" + str(100 * float(correct_dec_tree) / float(len(test_labels))) + 
+      "%).")
